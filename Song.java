d91d9fe969b4342a3155e53f350efe4710527567
Josh Brown <brownjoshalan@gmail.com>
2021-07-26 11:58:45 -0600

    Created the initial song object

---
+++ Song.java
@@ -0,0 +1,21 @@
+public class Song {
+    private String title;
+    private double length;
+
+    public Song(String title, double length) {
+        this.title = title;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public String toString() {
+        return title + "" + length;
+    }
+}
